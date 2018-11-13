9bb3ca5b848ae4e41a5856b6e70ac8ff51ea68ef
ALI HYSENI <ali.hyseni341@gmail.com>
2018-11-13 14:29:47 +0100

    fixing colums

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
 <style>
-iframe{
-    margin:100px 0;
-}
+
 .contact-wrapper{
     margin-top:50px;
 }
@@ -22,7 +20,7 @@ iframe{
                 <p>{{$contact->phone}}</p>
                 <p>{{$contact->email}}</p>
             @endforeach
-        <div>
+        </div>
         <div class="col-md-8">
             <div class="kontakti-right">
                 <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11738.207760768055!2d21.162463918695128!3d42.64965783338949!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549ee51e2b8be5%3A0xc1a61266fd75271e!2sPublic+Services+Ministry!5e0!3m2!1sen!2s!4v1536237420801"
