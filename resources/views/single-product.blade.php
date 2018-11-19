f1aeedd2fbea5b66257ddf896b7f52a92cf40bba
ALI HYSENI <ali.hyseni341@gmail.com>
2018-11-19 16:52:19 +0100

    Modifided single-product.blade.php

--- resources/views/single-product.blade.php
+++ resources/views/single-product.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-md-10">
                 @foreach($product as $product)
-                    <div class="produkt">
+                    <div class="product">
                         <p>{{ $product->name }}</p>
                         <img src="{{ asset('images')}}/{{$product->image}}" alt="{{$product->name}}">
                         <h5>Description</h5>
@@ -20,19 +20,11 @@
                         <h5>Usage</h5>
                         <p>{{ $product->usage }}</p>
                         <h5>Price</h5>
-                        <p>{{ $product->price}}</p>
+                        <p>{{ $product->price }}</p>
                     </div>
                 @endforeach
             </div>
         </div>
     </div>
 
-@endsection
-
-@section('scripts')
-<script>
-    $(document).ready(function(){
-        $(".product:first-of-type").remove();
-    });
-</script>
 @endsection
