cc005f83c645a7c4c6add73e36317a3433afbbcb
getoarll <ghetto.-@hotmail.com>
2018-11-27 11:11:22 +0100

    updated ProductsTableSeeder.php

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -66,7 +66,7 @@ class ProductsTableSeeder extends Seeder
         ]);
 
         \App\Products::insert([
-            'brands_id'=>'4',
+            'brands_id'=>'5',
             'name' => 'Vitamin C',
             'description'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book . It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged . It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum .',
             'ingredients'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
@@ -144,7 +144,7 @@ class ProductsTableSeeder extends Seeder
         ]);
 
         \App\Products::insert([
-            'brands_id'=>'4',
+            'brands_id'=>'5',
             'name' => 'Vitamin E',
             'description'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book . It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged . It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum .',
             'ingredients'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
@@ -170,7 +170,7 @@ class ProductsTableSeeder extends Seeder
         ]);
 
         \App\Products::insert([
-            'brands_id'=>'4',
+            'brands_id'=>'5',
             'name' => 'Creatine',
             'description'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book . It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged . It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum .',
             'ingredients'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
